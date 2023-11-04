44cffe647a3efb8df94c50f9ca05197d8c0ae91f
Anthony Bon Louis Uy Cubillas <66798713+louisuy@users.noreply.github.com>
2023-11-04 15:08:00 +0400

    Semi-working API, but still buggy.

--- app/api/employees/route.js
+++ app/api/employees/route.js
@@ -9,15 +9,34 @@ export async function POST(request) {
   const hire = await request.json();
 
   Object.entries(hire).forEach(([name, supervisor]) => {
-    const newHire = { [name]: [] };
-    if (Object.keys(employees).includes(supervisor)) {
-      console.log(`${supervisor} will supervise ${name}.`); 
-    } else if (
-      Object.values(employees).some((arr) =>
-        arr.some((employee) => Object.keys(employee)[0] === supervisor)
-      )
-    ) {
-      console.log(`${supervisor} will supervise ${name}.`);
+    const topLevelSupervisor = Object.keys(employees).includes(supervisor);
+    const supervisedSupervisor = Object.values(employees).some((arr) =>
+      arr.some((employee) => Object.keys(employee)[0] === supervisor)
+    );
+    const employeeExists = Object.values(employees).some((arr) =>
+      arr.some((employee) => Object.keys(employee)[0] === name)
+    );
+
+    if (employeeExists) {
+      console.log(`${name} is already an existing employee.`);
+    } else if (topLevelSupervisor) {
+      employees[supervisor].push({ [name]: [] });
+      console.log(`${supervisor} (top level) will supervise ${name}.`);
+    } else if (supervisedSupervisor) {
+      Object.values(employees).forEach((arr) => {
+        arr.forEach((employee) => {
+          const [supervisorName, supervised] = Object.entries(employee)[0];
+          if (supervisorName === supervisor) {
+            supervised.push({ [name]: [] });
+            console.log(`${supervisor} (supervised) will supervise ${name}.`);
+          } else {
+            Object.values(supervised).forEach((nestedEmployee) => {
+              nestedEmployee.hasOwnProperty(supervisor) &&
+                nestedEmployee[supervisor].push({ [name]: [] });
+            });
+          }
+        });
+      });
     } else {
       console.log(`${supervisor} is going to be a top level supervisor.`);
     }
