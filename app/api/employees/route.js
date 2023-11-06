999076aad9e480a549bd48d8047d2d9a2f21979a
Anthony Bon Louis Uy Cubillas <66798713+louisuy@users.noreply.github.com>
2023-11-06 16:23:05 +0400

    Patching functionality added.

--- app/api/employees/route.js
+++ app/api/employees/route.js
@@ -107,51 +107,53 @@ export async function POST(request) {
   return Response.json(employees, { status: 201 });
 }
 
+function deleteFromTeam(data, employee, supervisor) {
+  // console.log(employee, supervisor)
+  for (const key in data) {
+    // if (key === employee) {
+    //   console.log(data[key])
+    //   // delete data[key]; // Delete the employee entry from the object
+    //   return;
+    // }
+    if (data.hasOwnProperty(key)) {
+      if (key === supervisor) {
+        const members = data[key];
+        const employeeIndex = members.findIndex((member) =>
+          member.hasOwnProperty(employee)
+        );
+        if (employeeIndex !== -1) {
+          // console.log(members[employeeIndex])
+          members.splice(employeeIndex, 1); // Remove the employee from the array
+        }
+      }
+      if (Array.isArray(data[key])) {
+        data[key].forEach((subTeam) => {
+          deleteFromTeam(subTeam, employee, supervisor);
+        });
+
+        // Check if the array becomes empty after deleting the employee
+        // if (data[key].length === 0) {
+        //   // delete data[key]; // Delete the array if it becomes empty
+        // }
+      }
+    }
+  }
+}
+
+function deleteFromUiObject(data, employee) {
+  for (let i = 0; i < data.length; i++) {
+    const employeeData = data[i];
+    if (employeeData.name === employee) {
+      data.splice(i, 1); // Remove the employee object from the array
+      return;
+    }
+  }
+}
+
 export async function DELETE(request) {
   const employeesToDelete = await request.json();
   const employees = readDataFromFile(employeesPath);
   let employeesUiObject = readDataFromFile(employeesUiObjectPath);
-
-  function deleteFromTeam(data, employee, supervisor) {
-    // console.log(employee, supervisor)
-    for (const key in data) {
-      // if (key === employee) {
-      //   console.log(data[key])
-      //   // delete data[key]; // Delete the employee entry from the object
-      //   return;
-      // }
-      if (data.hasOwnProperty(key)) {
-        if (key === supervisor) {
-          const members = data[key];
-          const employeeIndex = members.findIndex((member) => member.hasOwnProperty(employee));
-          if (employeeIndex !== -1) {
-            // console.log(members[employeeIndex])
-            members.splice(employeeIndex, 1); // Remove the employee from the array
-          }
-        }
-        if (Array.isArray(data[key])) {
-          data[key].forEach((subTeam) => {
-            deleteFromTeam(subTeam, employee, supervisor);
-          });
-
-          // Check if the array becomes empty after deleting the employee
-          // if (data[key].length === 0) {
-          //   // delete data[key]; // Delete the array if it becomes empty
-          // }
-        }
-      }
-    }
-  }
-
-  function deleteFromUiObject(data, employee) {
-    for (let i = 0; i < data.length; i++) {
-      const employeeData = data[i];
-      if (employeeData.name === employee) {
-        data.splice(i, 1); // Remove the employee object from the array
-        return;
-      }
-    }
-  }
 
   Object.entries(employeesToDelete).forEach(([name, supervisor]) => {
     deleteFromTeam(employees, name, supervisor);
@@ -166,52 +168,41 @@ export async function DELETE(request) {
 }
 
 export async function PATCH(request) {
-  const editedEmployeeData = await request.json();
+  // const editedData = [{ "Moied": "Aysar" }, { "Louis": "Roubi" }];
+  const editedData = await request.json();
   const employees = readDataFromFile(employeesPath);
   let employeesUiObject = readDataFromFile(employeesUiObjectPath);
 
-  function findEmployee(data, employeeName) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].name === employeeName) {
-        return data[i];
-      }
-    }
-    return null;
-  }
+  let oldData, newData;
 
-  function updateEmployeeData(employee, editedData) {
-    for (const field in editedData) {
-      if (editedData.hasOwnProperty(field)) {
-        employee[field] = editedData[field];
-      }
-    }
-  }
-
-  function updateUiObject(data, employeeName, editedData) {
-    for (let i = 0; i < data.length; i++) {
-      const employeeData = data[i];
-      if (employeeData.name === employeeName) {
-        for (const field in editedData) {
-          if (editedData.hasOwnProperty(field)) {
-            employeeData[field] = editedData[field];
-          }
-        }
-        return;
-      }
-    }
-  }
-
-  Object.entries(editedEmployeeData).forEach(([name, editedData]) => {
-    const employee = findEmployee(employees, name);
-    if (employee) {
-      updateEmployeeData(employee, editedData);
-      updateUiObject(employeesUiObject, name, editedData);
+  editedData.forEach((modification, index) => {
+    if (index === 0) {
+      oldData = modification;
+    } else if (index === 1) {
+      newData = modification;
     }
   });
 
-  // Write the modified data back to the files
+  // console.log(oldData);
+  const oldName = Object.keys(oldData)[0];
+  const oldSupervisor = Object.values(oldData)[0];
+  console.log(oldName, oldSupervisor);
+
+  deleteFromTeam(employees, oldName, oldSupervisor);
   writeDataToFile(employees, employeesPath);
+  deleteFromUiObject(employeesUiObject, oldName);
   writeDataToFile(employeesUiObject, employeesUiObjectPath);
 
-  return Response.json(employees, { status: 200 });
+
+  console.log(newData);
+  const newName = Object.keys(newData)[0];
+  const newSupervisor = Object.values(newData)[0];
+
+  console.log(newName, newSupervisor);
+  iterateEmployees(newSupervisor, newName, employees);
+  writeDataToFile(employees, employeesPath);
+  updateEmployeesObject(newName, newSupervisor, employeesUiObject);
+  writeDataToFile(employeesUiObject, employeesUiObjectPath);
+
+  return Response.json(employees, { status: 201 });
 }
--- components/Edit.tsx
+++ components/Edit.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -10,50 +12,107 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 type EditDialogProps = {
   employee: { name: string; supervisor: string };
 };
 
 export function EditDialog({ employee }: EditDialogProps) {
+  const router = useRouter();
+
+  const oldName = employee.name;
+  const oldSupervisor = employee.supervisor;
+  let [name, setName] = useState("");
+  let [supervisor, setSupervisor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const addEmployee = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    if (name == "") {
+      name = oldName
+    }
+    if (supervisor == "") {
+      supervisor = oldSupervisor
+    }
+
+    setIsLoading(true);
+
+    const editedData = [
+      {
+        [oldName]: oldSupervisor,
+      },
+      {
+        [name]: supervisor,
+      },
+    ];
+
+    const res = await fetch("/api/employees", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(editedData),
+    });
+
+    if (res.status === 201) {
+      router.refresh();
+      router.push("/");
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit</DialogTitle>
-          <DialogDescription>
-            Are you sure you want to delete this employee?
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue={employee.name}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="supervisor" className="text-right">
-              Supervisor
-            </Label>
-            <Input
-              id="supervisor"
-              defaultValue={employee.supervisor}
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Confirm</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit Employee</DialogTitle>
+            <DialogDescription>Edit an employee in your team.</DialogDescription>
+          </DialogHeader>
+          <form onSubmit={addEmployee}>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  className="col-span-3"
+                  defaultValue={oldName}
+                  value={name}
+                  placeholder={oldName}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="supervisor" className="text-right">
+                  Supervisor
+                </Label>
+                <Input
+                  id="supervisor"
+                  className="col-span-3"
+                  defaultValue={oldSupervisor}
+                  value={supervisor}
+                  placeholder={oldSupervisor}
+                  onChange={(e) => setSupervisor(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <DialogFooter>
+              <Button type="submit" disabled={isLoading}>
+                {isLoading ? <span>Saving...</span> : <span>Save changes</span>}
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
